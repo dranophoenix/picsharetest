d88765ff93380dbebbe56a9745a1414eac284237
Kriangsak <kriangsak@Tanawats-MacBook-Pro.local>
2020-04-06 16:15:43 +0700

    src

---
+++ src/Picshare.elm
@@ -0,0 +1,34 @@
+module Picshare exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
+
+
+viewDetailedPhoto : String -> (String -> Html msg)
+viewDetailedPhoto url caption =
+    div [ class "detailed-photo" ]
+        [ img [ src url ] []
+        , div [ class "photo-info" ]
+            [ h2 [ class "caption" ]
+                [ text caption
+                ]
+            ]
+        ]
+
+
+baseUrl : String
+baseUrl =
+    "https://programming-elm.com/"
+
+
+main : Html msg
+main =
+    div []
+        [ div [ class "header" ] [ h1 [] [ text "Picshare" ] ]
+        , div [ class "content-flow" ]
+            [ viewDetailedPhoto (baseUrl ++ "1.jpg") "Surfing"
+            , viewDetailedPhoto (baseUrl ++ "2.jpg") "What doew the fox say?"
+            , viewDetailedPhoto (baseUrl ++ "3.jpg") "tree"
+            , viewDetailedPhoto (baseUrl ++ "4.jpg") "hooray"
+            ]
+        ]
